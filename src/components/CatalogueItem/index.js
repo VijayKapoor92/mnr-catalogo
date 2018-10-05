f3e8c7b4d07b27ef2513c7fd5161dec0dbbc6a5d
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-04 23:11:57 -0300

    styles: Muda estilo

--- src/components/CatalogueItem/index.js
+++ src/components/CatalogueItem/index.js
@@ -1,5 +1,6 @@
 import React, {Fragment} from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import MonetizationOn from '@material-ui/icons/MonetizationOn';
@@ -8,24 +9,27 @@ import {withStyles} from '@material-ui/core/styles';
 
 const CatalogueItem= ({classes, image, title, subheading, price, promotion}) => {
     return (
-        <Grid item lg={3} zeroMinWidth style={{position: 'relative'}}>
+        <Grid item xs={4} zeroMinWidth style={{position: 'relative'}}>
             {promotion.length > 0 && (
-                <MonetizationOn style={{position: 'absolute', top: 20, right: 20, zIndex: 1200, fontSize: 28, color: 'red'}}/>
+                <MonetizationOn style={{position: 'absolute', top: 10, right: 10, zIndex: 1200, fontSize: 28, color: 'red'}}/>
             )}
-            <div style={{padding: 20}}>
-                <div className={classes.paper}>
+            <div style={{margin: 20}}>
+                <div className={classNames(classes.paper, classes.paperDisplay)}>
                     {/*<div style={{position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, backgroundColor: 'rgba(900, 900, 900, .9)', zIndex: 1000}} />*/}
-                    <div className={classes.imageItem} style={{backgroundImage: `url(${image})`}}/>
-                    <div className={classes.content}>
-                        <Typography align="center">{title}</Typography>
-                        <Typography align="center" style={{fontSize: 12, color: '#757575'}}>{subheading}</Typography>
+                    {/*<div className={classes.imageItem} style={{backgroundImage: `url(${image})`}}/>*/}
+                    <img className={classes.imageItem} src={image} alt={title} />
+                    <div className={classNames(classes.content, classes.contentDisplay)}>
+                        <Typography align="center" variant="title" noWrap>{title}</Typography>
+                        <Typography align="center" variant="subheading" className={classes.subheading}>{subheading}</Typography>
                         {promotion ? (
                             <Fragment>
-                                <Typography align="center" noWrap style={{fontSize: 11, color: '#BDBDBD', textDecoration: 'line-through'}}>De: R${price}</Typography>
-                                <Typography align="center" noWrap>Por: R${promotion}</Typography>
+                                <Typography component="div" style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
+                                    <div style={{fontSize: 14, color: '#BDBDBD', textDecoration: 'line-through', textAlign: 'center'}}><span style={{fontSize: 12}}>R$&nbsp;</span>{price}</div>
+                                    <div style={{textAlign: 'center', fontSize: 24}}><span style={{fontSize: 12}}>R$&nbsp;</span>{promotion}</div>
+                                </Typography>
                             </Fragment>
                         ) : (
-                            <Typography align="center" noWrap>R${price}</Typography>
+                            <Typography align="center" noWrap className={classes.price}><span style={{fontSize: 12}}>R$&nbsp;</span>{price}</Typography>
                         )}
                     </div>
                 </div>
