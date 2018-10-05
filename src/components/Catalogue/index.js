82be1c195007e0c482a1ebf602cfdf35d994d717
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-05 15:09:02 -0300

    feat: Implementa prop mouse e suas funcoes

--- src/components/Catalogue/index.js
+++ src/components/Catalogue/index.js
@@ -4,14 +4,22 @@ import CatalogueList from '../CatalogueList';
 import {styles} from './styles';
 import {withStyles} from "@material-ui/core/styles/index";
 
-const Catalogue = ({classes, catalogue}) => (
+const Catalogue = ({classes, catalogue, onMouseEnter, onMouseLeave, mouse}) => (
     <div className={classes.root}>
-        <CatalogueList catalogue={catalogue}/>
+        <CatalogueList
+            catalogue={catalogue}
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            mouse={mouse}
+        />
     </div>
 );
 
 Catalogue.propTypes = {
-    catalogue: PropTypes.array.isRequired
+    catalogue: PropTypes.array.isRequired,
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    mouse: PropTypes.string
 };
 
 export default withStyles(styles)(Catalogue);
--- src/components/CatalogueList/index.js
+++ src/components/CatalogueList/index.js
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 import Grid from '@material-ui/core/Grid';
 import CatalogueItem from '../CatalogueItem';
 
-const CatalogueList = ({classes, catalogue}) => {
+const CatalogueList = ({classes, catalogue, onMouseEnter, onMouseLeave, mouse}) => {
     return (
-        <Grid container>
+        <Grid container spacing={24}>
             {catalogue.map(({id, nm_produto, descricao, vl_unitario, vl_promocional, nm_foto}) => (
                 <CatalogueItem
                     key={id}
@@ -14,6 +14,9 @@ const CatalogueList = ({classes, catalogue}) => {
                     subheading={descricao}
                     price={vl_unitario}
                     promotion={vl_promocional}
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                    mouse={mouse}
                 />
             ))}
         </Grid>
@@ -21,7 +24,10 @@ const CatalogueList = ({classes, catalogue}) => {
 };
 
 CatalogueList.propTypes = {
-    catalogue: PropTypes.array.isRequired
+    catalogue: PropTypes.array.isRequired,
+    onMouseEnter: PropTypes.func,
+    onMouseLeave: PropTypes.func,
+    mouse: PropTypes.string
 };
 
 export default CatalogueList;
