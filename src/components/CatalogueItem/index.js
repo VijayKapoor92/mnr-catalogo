1fd6ae809ed7d465b374746a552ac75692af81ec
Vijay Kapoor <vijayk_1@hotmail.com>
2018-10-05 15:08:30 -0300

    feat: Implementa os typos

--- src/components/CatalogueItem/index.js
+++ src/components/CatalogueItem/index.js
@@ -3,35 +3,31 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { Title, SubHeading } from '../Typos';
+import Price from '../Price';
 import MonetizationOn from '@material-ui/icons/MonetizationOn';
 import { styles } from './styles';
 import {withStyles} from '@material-ui/core/styles';
 
-const CatalogueItem= ({classes, image, title, subheading, price, promotion}) => {
+const ItemInfo = withStyles(styles)(({classes, mouse}) =>
+    <div className={classNames(classes.itemInfo, mouse === 'in' && classes.itemInfoHover)} />
+);
+
+const CatalogueItem= ({classes, image, title, subheading, price, promotion, onMouseEnter, onMouseLeave, mouse}) => {
     return (
         <Grid item xs={4} zeroMinWidth style={{position: 'relative'}}>
             {promotion.length > 0 && (
                 <MonetizationOn style={{position: 'absolute', top: 10, right: 10, zIndex: 1200, fontSize: 28, color: 'red'}}/>
             )}
-            <div style={{margin: 20}}>
-                <div className={classNames(classes.paper, classes.paperDisplay)}>
-                    {/*<div style={{position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, backgroundColor: 'rgba(900, 900, 900, .9)', zIndex: 1000}} />*/}
-                    {/*<div className={classes.imageItem} style={{backgroundImage: `url(${image})`}}/>*/}
-                    <img className={classes.imageItem} src={image} alt={title} />
-                    <div className={classNames(classes.content, classes.contentDisplay)}>
-                        <Typography align="center" variant="title" noWrap>{title}</Typography>
-                        <Typography align="center" variant="subheading" className={classes.subheading}>{subheading}</Typography>
-                        {promotion ? (
-                            <Fragment>
-                                <Typography component="div" style={{width: '100%', display: 'flex', flexDirection: 'column'}}>
-                                    <div style={{fontSize: 14, color: '#BDBDBD', textDecoration: 'line-through', textAlign: 'center'}}><span style={{fontSize: 12}}>R$&nbsp;</span>{price}</div>
-                                    <div style={{textAlign: 'center', fontSize: 24}}><span style={{fontSize: 12}}>R$&nbsp;</span>{promotion}</div>
-                                </Typography>
-                            </Fragment>
-                        ) : (
-                            <Typography align="center" noWrap className={classes.price}><span style={{fontSize: 12}}>R$&nbsp;</span>{price}</Typography>
-                        )}
-                    </div>
+
+            <div className={classNames(classes.paper, classes.paperDisplay)} onMouseEnter={e => onMouseEnter(e)} onMouseLeave={e => onMouseLeave(e)}>
+                <ItemInfo mouse={mouse}/>
+                {/*<div className={classes.imageItem} style={{backgroundImage: `url(${image})`}}/>*/}
+                <img className={classes.imageItem} src={image} alt={title} />
+                <div className={classNames(classes.content, classes.contentDisplay)}>
+                    <Title text={title} align="center" noWrap bold/>
+                    <SubHeading text={subheading} align="center" bold/>
+                    <Price prefix="R$" price={price} promotionPrice={promotion} promotion={promotion}/>
                 </div>
             </div>
         </Grid>
